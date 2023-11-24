5d8a1415a859654a80f36f20d9991f9b22f86ca5
aditya <aditya.dubey@opensenselabs.com>
2023-11-24 11:31:58 +0530

    updated

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import './style.scss'
-const PageNotFound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default PageNotFound
+import React from "react";
+
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
+
